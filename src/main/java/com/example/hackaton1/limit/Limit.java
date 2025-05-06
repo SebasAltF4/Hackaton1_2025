7fc5e064fcb349b2fb17b7142bb5445a4496e44e
Oscar Ramirez <oscar.ramirez.e@utec.edu.pe>
2025-05-06 17:23:57 -0500

    entidades

--- src/main/java/com/example/hackaton1/limit/Limit.java
+++ src/main/java/com/example/hackaton1/limit/Limit.java
@@ -1,4 +1,30 @@
 package com.example.hackaton1.limit;
 
+import com.example.hackaton1.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Duration;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Limit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String modelName;
+    private Integer maxTokens;
+    private Integer maxRequests;
+    private Duration timeWindow;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 }
---
+++ src/main/java/com/example/hackaton1/user/Role.java
@@ -0,0 +1,5 @@
+package com.example.hackaton1.user;
+
+public enum Role {
+    ROLE_SPARKY_ADMIN, ROLE_COMPANY_ADMIN, ROLE_USER
+}
